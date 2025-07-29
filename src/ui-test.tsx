2686872c26607e32c432fc14a6498bb2bc4f97f5
ryuusei1 <ryuusei1@ryuusei1noMacBook-Air.local>
2025-07-29 13:21:25 +0900

    ボタンのUIの基礎を作成

--- src/ui-test.tsx
+++ src/ui-test.tsx
@@ -1,5 +1,32 @@
 export default function UITest() {
-	return <div>
-    <button type="button">ペン</button>
-  </div>;
+	return <div id="mainbox">
+    <div id="firstbox">
+    <button type="button" className="basebtn">
+      <img src="src/icons/pen.svg" alt="ペンアイコン" />
+      ペン
+      </button>
+    <button type="button" className="basebtn">
+      <img src="src/icons/eraser.svg" alt="消しゴムアイコン" className="svg-icon" />
+      消しゴム
+      </button>
+    <button type="button" className="basebtn">
+      <img src="src/icons/lasso.svg" alt="投げ縄選択アイコン" className="svg-icon" />
+      投げ縄選択
+      </button>
+    
+    <button type="button" className="btn-selected">
+      <img src="src/icons/ban.svg" alt="選択用アイコン" className="svg-icon" />
+    </button>
+  </div>
+  <div id="secondbox">
+    <button type="button" className="basebtn">
+      <img src="src/icons/trash-2.svg" alt="リセットアイコン" className="svg-icon" />
+      リセット
+    </button>
+    <button type="button" className="basebtn">
+      <img src="src/icons/undo-2.svg" alt="元に戻すアイコン" className="svg-icon" />
+      元に戻す
+    </button>
+  </div>
+  </div>
 }
