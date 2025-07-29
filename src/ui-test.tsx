1b6ba3f6916808e24e9f7b92acf1a1650299319a
ryuusei1 <ryuusei1@ryuusei1noMacBook-Air.local>
2025-07-29 16:09:41 +0900

    ボタン配置訂正

--- src/ui-test.tsx
+++ src/ui-test.tsx
@@ -1,24 +1,62 @@
 export default function UITest() {
-	return <div id="mainbox">
-    <div id="firstbox">
+	return <>
+    <Header />
+
+    <div id="mainbox">
+      <FirstBox />
+
+      <div id="thirdbox">
+      </div>
+
+      <SecondBox />
+    </div>
+  </>;
+}
+
+function Header() {
+  return <header id="header">
+    落書きシティ
+  </header>
+}
+
+function ToolButton(props: {src: string, name: string, selected: boolean}) { 
+  return <button type="button" className="basebtn">
+    <img
+      src={props.src}
+      alt={`${props.name}アイコン`}
+      className={`svg-icon ${props.selected ? "btn-selected" : ""}`}
+    />
+    {props.name}
+  </button>
+}
+
+// - ロゴの配置
+// - 上のToolButtonを少し勉強する（任意）
+
+function FirstBox() {
+  return <div id="firstbox">
     <button type="button" className="basebtn">
       <img src="src/icons/pen.svg" alt="ペンアイコン" className="svg-icon" />
       ペン
-      </button>
+    </button>
+    
     <button type="button" className="basebtn">
       <img src="src/icons/eraser.svg" alt="消しゴムアイコン" className="svg-icon" />
       消しゴム
-      </button>
-    <button type="button" className="basebtn">
-      <img src="src/icons/lasso.svg" alt="投げ縄選択アイコン" className="svg-icon" />
-      投げ縄選択
-      </button>
-    
-    <button type="button" className="basebtn">
-      <img src="src/icons/ban.svg" alt="選択用アイコン" className="svg-icon btn-selected " />
     </button>
-  </div>
-  <div id="secondbox">
+    <button type="button" className="basebtn">
+      <img src="src/icons/lasso.svg" alt="投げ縄選択アイコン" className="svg-icon btn-selected" />
+      投げ縄選択
+    </button>
+  
+    {/* <button type="button" className="basebtn">
+      <img src="src/icons/ban.svg" alt="選択用アイコン" className="svg-icon btn-selected " />
+    </button> */}
+  </div>;
+}
+
+function SecondBox() {
+  return <div id="secondbox">
     <button type="button" className="basebtn">
       <img src="src/icons/trash-2.svg" alt="リセットアイコン" className="svg-icon" />
       リセット
@@ -27,6 +65,5 @@ export default function UITest() {
       <img src="src/icons/undo-2.svg" alt="元に戻すアイコン" className="svg-icon" />
       元に戻す
     </button>
-  </div>
-  </div>
+  </div>;
 }
