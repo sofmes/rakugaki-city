ded5c43d3b3113831c1f22104cf70576e5132620
Takagi Tasuku <tasuren@outlook.jp>
2025-08-26 16:04:20 +0900

    少し使い勝手が悪かったので、`sanitize.css`に移行

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,18 +2,17 @@ import { jsxRenderer } from "hono/jsx-renderer";
 import { Link, ViteClient } from "vite-ssr-components/hono";
 
 export const renderer = jsxRenderer(({ children }) => {
-	return (
-		<html lang="ja">
-			<head>
-				<ViteClient />
-				<Link href="/src/style.css" rel="stylesheet" />
-				<Link href="/node_modules/modern-normalize/modern-normalize.css" rel="stylesheet" />
-				<title>落書きシティ</title>
+  return (
+    <html lang="ja">
+      <head>
+        <ViteClient />
+        <Link href="/src/style.css" rel="stylesheet" />
+        <title>落書きシティ</title>
 
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			</head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
 
-			<body>{children}</body>
-		</html>
-	);
+      <body>{children}</body>
+    </html>
+  );
 });
