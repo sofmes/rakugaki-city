296e139f1054645f927a652e3d73dcbf600c4188
Takagi Tasuku <tasuren@outlook.jp>
2025-09-23 15:35:32 +0900

    使われていないコンテキストを削除

--- src/components/client/context.tsx
+++ src/components/client/context.tsx
@@ -1,6 +1,4 @@
 import { createContext } from "hono/jsx";
-import type { CanvasState, RemoteCanvas } from "../../lib/client/session";
+import type { RemoteCanvas } from "../../lib/client/session";
 
 export const SessionContext = createContext<RemoteCanvas | null>(null);
-
-export const ConnectionStateContext = createContext<CanvasState>("closed");
